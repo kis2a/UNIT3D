a0e0aca2fc6917acc5b69cb8042e8d46690274ce
kis2a <95507076+kis2a@users.noreply.github.com>
2021-12-24 13:25:26 +0100

    achevienemetn lang

---
+++ resources/lang/fr/Achievements.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+
+/*REQUESTS*/
+
+return [
+    /*REQUESTS*/
+          '100requests'      => 'DAMN BRO! You have already filled 100 requests!',
+          '75requests'      => 'DAMN BRO! You have already filled 75 requests!',
+          '50requests'      => 'DAMN BRO! You have already filled 50 requests!',
+          '25requests'      => 'DAMN BRO! You have already filled 25 requests!',
+    /*COMMENTS*/
+          '100comments'      => 'Wow! You have already made 100 comments!',
+          '75comments'      => 'Wow! You have already made 75 comments!',
+          '50comments'      => 'Wow! You have already made 50 comments!',
+          '25comments'      => 'Wow! You have already made 25 comments!',
+    /*POSTS*/
+          '100posts'      => 'Wow! You have already made 100 posts!',
+          '75posts'      => 'Wow! You have already made 75 posts!',
+          '50posts'      => 'Wow! You have already made 50 posts!',
+          '25posts'      => 'Wow! You have already made 25 posts!',
+    /*UploadS*/
+           '100uploads'      => 'Wow! You have already made 100 uploads!',
+           '75uploads'      => 'Wow! You have already made 75 uploads!',
+          '50uploads'      => 'Wow! You have already made 50 uploads!',
+          '25uploads'      => 'Wow! You have already made 25 uploads!',
+   /*Subtitles*/
+  
+   
+];
